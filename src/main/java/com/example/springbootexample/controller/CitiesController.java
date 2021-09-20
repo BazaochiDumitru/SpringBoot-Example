3d293db2bd4f0f6fa5f93aee0e803d38187e9f79
BazaochiDumitru <bazaochi.dumitru@mail.ru>
2021-09-20 12:24:52 +0300

    Create Cities method

--- src/main/java/com/example/springbootexample/controller/CitiesController.java
+++ src/main/java/com/example/springbootexample/controller/CitiesController.java
@@ -1,4 +1,18 @@
 package com.example.springbootexample.controller;
 
+import com.example.springbootexample.repository.CitiesRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CitiesController {
+
+    CitiesRepository citiesRepository = new CitiesRepository();
+
+    @PostMapping("")
+    public String create(@RequestParam String citiesName){
+        return citiesRepository.create(citiesName);
+    }
+
 }
--- src/main/java/com/example/springbootexample/repository/CitiesRepository.java
+++ src/main/java/com/example/springbootexample/repository/CitiesRepository.java
@@ -1,4 +1,20 @@
 package com.example.springbootexample.repository;
 
+import java.util.ArrayList;
+
 public class CitiesRepository {
+
+    ArrayList<String> countries = new ArrayList<>();
+
+    public CitiesRepository(){
+        this.countries.add("Moldova");
+        this.countries.add("Ucraina");
+        this.countries.add("Romania");
+        this.countries.add("Turkey");
+    }
+
+    public String create(String countryName){
+        this.countries.add(countryName);
+        return "Create Country";
+    }
 }
